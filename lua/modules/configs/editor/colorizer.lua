50fa121846994de7e6655260a30931aa9dc1c436
YanTree <yantreeland@outlook.com>
2023-04-17 13:05:14 +0800

    update: disable highlight string(eg: blue) for colorizer

--- lua/modules/configs/editor/colorizer.lua
+++ lua/modules/configs/editor/colorizer.lua
@@ -1,3 +1,13 @@
 return function()
-        require("colorizer").setup({})
+        require("colorizer").setup({
+                -- highlight on all files
+                filetypes = { "*" },
+                -- change default config
+                user_default_options = {
+                        -- disable highlight with string(eg: blue)
+                        names = false,
+                },
+                -- all the sub-options of filetypes apply to buftypes
+                -- buftypes = {},
+        })
 end
