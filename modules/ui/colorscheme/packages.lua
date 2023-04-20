77b0190ee5270481379bc9cdf5a5222e93ec350f
YanTree <yantreeland@outlook.com>
2023-04-20 18:09:12 +0800

    update: start new module system

---
+++ modules/ui/colorscheme/packages.lua
@@ -0,0 +1,12 @@
+--ui/colorscheme/packages.lua
+
+packages!["ellisonleao/gruvbox.nvim"] = {
+        pin = true,
+        commit = "42e92960a334c36cf588f096b5821bc63c98293d",
+}
+
+packages!["loctvl842/monokai-pro.nvim"] = {
+        pin = true,
+        commit = "939be5f7fcbc15abeaad9214bb253a4551f6f6e6",
+}
+
