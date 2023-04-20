ef116de847de089dc56a11ab8998e03a3a8650c0
YanTree <yantreeland@outlook.com>
2023-04-20 22:03:50 +0800

    update: add cmp test file of modules

---
+++ modules/ui/colorscheme/config.lua
@@ -0,0 +1,5 @@
+local colorscheme = require("config").colorscheme
+
+local options = {
+
+}
--- modules/ui/colorscheme/packages.lua
+++ modules/ui/colorscheme/packages.lua
@@ -1,12 +1,13 @@
---ui/colorscheme/packages.lua
+-- ui/colorscheme/packages.lua
 
-packages!["ellisonleao/gruvbox.nvim"] = {
+packages["ellisonleao/gruvbox.nvim"] = {
         pin = true,
         commit = "42e92960a334c36cf588f096b5821bc63c98293d",
 }
 
-packages!["loctvl842/monokai-pro.nvim"] = {
+packages["loctvl842/monokai-pro.nvim"] = {
         pin = true,
         commit = "939be5f7fcbc15abeaad9214bb253a4551f6f6e6",
 }
 
+print(vim.inspect(packages))
