c658d8764738b64c3546865bc45558401ce7fb34
YanTree <yantreeland@outlook.com>
2023-04-21 21:49:32 +0800

    update: test on packages.lua

--- modules/ui/colorscheme/packages.lua
+++ modules/ui/colorscheme/packages.lua
@@ -5,9 +5,19 @@ packages["ellisonleao/gruvbox.nvim"] = {
         commit = "42e92960a334c36cf588f096b5821bc63c98293d",
 }
 
-packages["loctvl842/monokai-pro.nvim"] = {
+local list = {
+        "packagename",
         pin = true,
-        commit = "939be5f7fcbc15abeaad9214bb253a4551f6f6e6",
+        tag = "0.2.1",
 }
 
-print(vim.inspect(packages))
+local module = {}
+
+for k, v in pairs(packages) do
+        module[#module+1] = vim.tbl_extend("force", {k}, v)
+        print(vim.inspect(k))
+        print(vim.inspect(v))
+        print(vim.inspect(module))
+end
+-- print(vim.inspect(packages))
+-- print(vim.inspect(list))
