876e67d2f6f90ff363ba55916bf7dc75f50301f2
YanTree <yantreeland@outlook.com>
2023-04-24 12:04:16 +0800

    update: disable funtion on main branch

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@ if not vim.g.vscode then
 	require("core")
 end
 
-require("init")
-
-local lazypath = require("config").path.data .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-        require("lazy").load_lazy(lazypath)
-end
-
-vim.opt.rtp:prepend(lazypath)
+-- require("init")
+--
+-- local lazypath = require("config").path.data .. "/lazy/lazy.nvim"
+--
+-- if not vim.loop.fs_stat(lazypath) then
+--         require("lazy").load_lazy(lazypath)
+-- end
+--
+-- vim.opt.rtp:prepend(lazypath)
