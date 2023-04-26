0daa50ca090b66f0695cef12ceb5156f35f2fafb
YanTree <yantreeland@outlook.com>
2023-04-26 22:03:39 +0800

    update: modify mappings

--- modules/editor/mappings/config.lua
+++ modules/editor/mappings/config.lua
@@ -1,5 +1,13 @@
 local maps = { i = {}, n = {}, v = {}, t = {} }
 
+local sections = {
+        f = { desc = "File"},
+        w = { desc = "Window"},
+        s = { desc = "Search"},
+        g = { desc = "Git"},
+        b = { desc = "Buffer"},
+}
+
 -- editor
 maps.n["<Esc>"]         = { ":noh <CR>",                    desc = "Clear highlights" }
 maps.v["p"]             = { '"_dP',                         desc = "Alway paste context from clipboard" }
@@ -9,13 +17,13 @@ maps.v["<"]             = { "<gv",                          desc = "Decrease ind
 maps.v[">"]             = { ">gv",                          desc = "Increase indent" }
 
 -- window
+-- maps.n["<Leader>w"]     = sections.w
 maps.n["<leader>wv"]    = { "<C-w>v",                       desc = "Split window vertically" }
 maps.n["<leader>wh"]    = { "<C-w>s",                       desc = "Split window horizontal" }
 maps.n["<C-h>"]         = { "<C-w>h",                       desc = "Move cursor to left window" }
-maps.n["<C-h>"]         = { "<C-w>h",                       desc = "Move cursor to left window" }
 maps.n["<C-l>"]         = { "<C-w>l",                       desc = "Move cursor to right window" }
-maps.n["<C-l>"]         = { "<C-w>j",                       desc = "Move cursor to down window" }
-maps.n["<C-l>"]         = { "<C-w>k",                       desc = "Move cursor to up window" }
+maps.n["<C-j>"]         = { "<C-w>j",                       desc = "Move cursor to down window" }
+maps.n["<C-k>"]         = { "<C-w>k",                       desc = "Move cursor to up window" }
 
 maps.n["<C-Up>"]        = { "<cmd>resize -2<CR>",           desc = "Resize split up" }
 maps.n["<C-Down>"]      = { "<cmd>resize +2<CR>",           desc = "Resize split down" }
